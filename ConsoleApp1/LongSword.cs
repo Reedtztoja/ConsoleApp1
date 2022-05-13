c54e9d1d5fc19286aa8df25c12e3a72d8468e02d
3111232 <3111232@TE-GDA-D-D37H01>
2022-05-13 14:11:55 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/LongSword.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LolClasses
+{
+    /*
+     * klasa opisująca konkretny rodzaj itemu w sklepie
+     */
+    public class LongSword : Item
+    {
+        //deprecated???
+        //public int bonusAD = 10;
+        public LongSword()
+        {
+            //ustalamy nazwe itemu
+            name = "Long Sword";
+            // jego cene
+            price = 350;
+            //i atrybuty
+            AddStat(Stat.AD, 10);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using LolClasses;
+
+//stwórz nowego champa - garena
+Garen g = new Garen();
+//opdal emotke dla garena
+g.ctrl3();
+
+//stworz instacje przedmiotu - rubycrystal, longsword
+RubyCrystal rb = new RubyCrystal();
+LongSword ls = new LongSword();
+//sprawdz ile ma hp garen
+g.showHP();
+g.showAD();
+//kup mu item
+g.buyItem(rb);
+//sprawdz ile ma teraz hp
+g.showHP();
+//kup mu drugi item
+g.buyItem(rb);
+g.buyItem(ls);
+//sprawdz ile ma teraz hp
+g.showHP();
+//sprawdz ile ma ad
+g.showAD();
+
+
+
+
+
+
